b5f10f6223ac8a3250b82c6ff5c06bed43711485
heba gamal <heba2491990@gmail.com>
2021-12-14 10:09:10 +0200

    Add files via upload

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,26 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
-            $table->string('phone');
+            $table->string('phone')->unique()->nullable();
+            $table->string('sex')->default('male');
+            $table->date('brithday')->nullable();
+            $table->text('address')->nullable();
+            $table->string('logo')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('isAdmin')->default('0');
+            $table->string('api_key')->nullable();
+            $table->unsignedBigInteger('country_id')->nullable();
+            $table->unsignedBigInteger('city_id')->nullable();
+            $table->unsignedBigInteger('area_id')->nullable();
+            $table->string('long')->nullable();
+            $table->string('lat')->nullable();
+            $table->integer('code')->nullable();
+            $table->boolean('activated')->default(false);
+            $table->boolean('blocked')->default(false);
+            $table->timestamp('last_login_at ')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
